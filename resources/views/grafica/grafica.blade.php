107d8a0af7a2cbc1cd2c3d9ecfbbdde62f626685
Chocobo98 <extremo98@live.com.mx>
2021-05-25 14:44:19 -0700

    Agregado de boton para descargar PDF

--- resources/views/grafica/grafica.blade.php
+++ resources/views/grafica/grafica.blade.php
@@ -13,6 +13,14 @@
   </div>
   @include('tablas.tablasNumerosTickets',['ticketMensual'=>$ticketMensual,'ticketGeneral'=>$ticketGeneral])
 </div>
+<div class="ml-10 mt-2 flex">
+  <a href="{{ route('pdf.download') }}">
+     <button class="bg-gray-300 hover:bg-gray-400 text-gray-900 font-bold py-2 px-4 rounded inline-flex items-center">
+        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+        <span>Generar reporte mensual</span>
+      </button>
+  </a>
+</div>
 
 <!-- Charting library -->
 <script src="https://unpkg.com/chart.js@2.9.3/dist/Chart.min.js"></script>
